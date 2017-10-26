e4ca201380bb8a693e5897518ffc71ad275c01c4
kevines <bloodylzy1994@gmail.com>
2017-10-26 13:58:18 +0800

    新增方法

--- src/main/java/com/cnwisdom/demo/HolleApi.java
+++ src/main/java/com/cnwisdom/demo/HolleApi.java
@@ -2,7 +2,6 @@ package com.cnwisdom.demo;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HolleApi {
 
 	@GetMapping("/")  
-    String home() {  
         return "Hello";  
     }
 }
